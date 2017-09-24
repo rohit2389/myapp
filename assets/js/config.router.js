9882dcd4a172f3a2629c7e4a50c3acfd9ddefe11
rohit_ajrh <rohit.shri2389@gmail.com>
2017-09-24 12:54:03 +0530

    nexted routing

--- assets/js/config.router.js
+++ assets/js/config.router.js
@@ -1,12 +1,20 @@
 
 App.config(['$stateProvider', '$urlRouterProvider', '$locationProvider', function($stateProvider, $urlRouterProvider, $locationProvider){
                 // For any unmatched url, send to /business
-    $urlRouterProvider.otherwise("/")
+    // $urlRouterProvider.otherwise("/")
      
     $stateProvider
-        .state('/', {
+        .state('app', {
         	url: '/',
         	templateUrl: 'assets/view/app.html'
+        })
+        .state('app.login', {
+        	url: '/login',
+        	templateUrl: 'assets/view/login.html'
+        })
+        .state('/dashboard', {
+        	url: '/',
+        	templateUrl: 'assets/view/dashboard.html'
         })
     $locationProvider.html5Mode(true);
 }]);
